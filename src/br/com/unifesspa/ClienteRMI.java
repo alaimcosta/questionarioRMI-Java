75aacd76f87f7c1b9768f2ae0bfb2760211d3d8e
alaimcosta <alaim.costa23@gmail.com>
2022-04-29 05:55:54 -0300

    Primeiro Commit

---
+++ src/br/com/unifesspa/ClienteRMI.java
@@ -0,0 +1,22 @@
+package br.com.unifesspa;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
+public class ClienteRMI {
+	 static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));;
+	
+	public static void main(String[] args) throws Exception {
+		String objName = "rmi://localhost:1099/Gab";
+		IGabarito gab = (IGabarito) Naming.lookup(objName);
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.println("Digite a resposta: ");
+			String s = bufferedReader.readLine();
+			System.out.println("Correçao pelo gabarito: " + gab.corrige(s));
+		}
+	}
+}
+
